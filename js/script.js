7581b1a5e5e8b7bd10567cf85e4aaa9e7df01221
splaa <splaandrey@gmail.com>
2021-01-26 08:14:09 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+var a  = 15
+let number = 5;
+const leftBorderWith = 1;
+number = 10;
+number = 12;
+number = 13;
+
+console.log(number);
+
+
+const obj =  {
+    a:50
+
+}
+
+console.log(obj.a = 15);
